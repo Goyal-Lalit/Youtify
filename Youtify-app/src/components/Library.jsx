21546c506d8ba3c06135594af82eac666f880b1f
abhii47 <150163008+abhii47@users.noreply.github.com>
2025-03-18 01:05:47 +0530

    add navbar and library compo

---
+++ Youtify-app/src/components/Library.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Library() {
+  return (
+    <div>
+      <h1>Your Library</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, repellat.</p>
+    </div>
+  )
+}
+
+export default Library
